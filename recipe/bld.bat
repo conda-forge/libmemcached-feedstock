172ba1bad57a6c11a0f7736b6da51795f21046a9
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-12-05 10:39:00 -0500

    use bash for configure on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,4 @@
-dir
-bash -lc "./configure --without-docs --prefix=`cygpath -u $PREFIX`" || goto :ERROR
+bash -lc "bash configure --without-docs --prefix=`cygpath -u $PREFIX`" || goto :ERROR
 bash -lc "make" || goto :ERROR
 bash -lc "make install" || goto :ERROR
 
